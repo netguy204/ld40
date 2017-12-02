3866bbec050059282b4ee8f646006694c1729f4b
Brian Taylor <brian@productlab.com>
2017-12-02 12:16:45 -0500

    initial import

---
+++ Assets/Scripts/FollowOnString.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowOnString : MonoBehaviour {
+    public GameObject Target; // what we follow
+    public float StringLength = 1.0f;
+
+    private float m_z;
+
+	// Use this for initialization
+	void Start () {
+        m_z = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        Vector2 towardTarget = (Target.transform.position - transform.position);
+        float distance = towardTarget.magnitude;
+        towardTarget.Normalize();
+
+        if (distance > StringLength)
+        {
+            float violation = distance - StringLength;
+
+            transform.position += violation * (Vector3)towardTarget;
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    public float JumpHeight = 10.0f;
+
+    private Rigidbody2D m_rigidBody;
+    private GameObject m_world;
+    private bool m_grounded = true;
+    private float m_jumpImpulse = 0.0f;
+
+	// Use this for initialization
+	void Start () {     
+        m_rigidBody = GetComponent<Rigidbody2D>();
+        m_world = GameObject.Find("World");
+        m_jumpImpulse = m_rigidBody.mass * Mathf.Sqrt(2.0f * Mathf.Abs(Physics.gravity.y) * JumpHeight / 3.0f);
+	}
+	
+    Vector3 MouseInWorld()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        return ray.origin;
+    }
+
+    Vector2 ToMouse()
+    {
+        Vector2 toMouse = MouseInWorld() - transform.position;
+        toMouse.Normalize();
+        return toMouse;
+    }
+
+    
+    void FixedUpdate () {
+        //bool jumpStart = Input.GetButtonDown("Jump");
+        bool jumpPressed = Input.GetButton("Jump");
+
+        if (m_grounded && jumpPressed)
+        {
+            Vector2 jumpForce = m_jumpImpulse * ToMouse();
+            Debug.Log(jumpForce.ToString());
+            m_rigidBody.AddForce(jumpForce, ForceMode2D.Impulse);
+            m_grounded = false;
+            m_rigidBody.gravityScale = 1.0f;
+        }
+	}
+
+    // Update is called once per frame
+    private void Update()
+    {
+        Vector2 rayEnd = (Vector2)transform.position + 3.0f * ToMouse();
+        Debug.DrawLine(transform.position, rayEnd, Color.red);
+    }
+
+    void UpdateGrounded(Collider2D collider)
+    {
+        GameObject go = collider.gameObject;
+        while (go != null)
+        {
+            if (go == m_world)
+            {
+                Debug.Log("triggered");
+                m_grounded = true;
+                m_rigidBody.velocity = Vector2.zero;
+                m_rigidBody.gravityScale = 0.0f;
+                return;
+            }
+
+            go = go.transform.parent.gameObject;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        UpdateGrounded(collision.collider);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("on collision");
+        UpdateGrounded(collision.collider);
+    }
+}
